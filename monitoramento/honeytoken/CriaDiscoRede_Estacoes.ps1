8e96b0c301ca63264bd4f8ea2ed9b029b6443dcd
André Torres <84332391+andretorresbr@users.noreply.github.com>
2026-01-04 22:47:09 -0300

    Create CriaDiscoRede_Estacoes.ps1

---
+++ monitoramento/honeytoken/CriaDiscoRede_Estacoes.ps1
@@ -0,0 +1,25 @@
+# Script em fase de testes, favor não alterar
+$networkPath = "\\srv-fileserver\usuarios"
+$driveLetter = "X:"
+$username = "corp\svc_wkstasks"
+$password = "test3@tar3f4$"
+
+# Create a secure password
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+
+# Create a PSCredential object
+$credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
+
+# Remove o mapeamento, se já existir
+if (Test-Path $driveLetter) {
+    Write-Host "Drive $driveLetter ja existe. Removendo..."
+    Remove-PSDrive -Name $driveLetter -Force -ErrorAction SilentlyContinue
+}
+
+# Mapeia o driver de rede
+try {
+    New-PSDrive -Name $driveLetter.TrimEnd(':') -PSProvider FileSystem -Root $networkPath -Credential $credential -Persist
+    Write-Host "Drive $driveLetter mapeado com sucesso para $networkPath."
+} catch {
+    Write-Host "Falha para mapear o drive. Erro: $_"
+}
